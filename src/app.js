070543a865490ebf795df625f16b70b1f626dda2
Juliana Sartori <juliana.sartori@rethink.dev>
2023-07-03 15:03:00 -0300

    routes
    principal rotas

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/center-panel" element={<CenterPanel />} />
+          <Route path="/analysis" element={<Analysi />} />
+          <Route path="/contact" element={<Contac />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+          <Route path="/map" element={<Map />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
